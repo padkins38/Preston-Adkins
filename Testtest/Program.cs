4c4a5c91748e9d5771a797c05bc4c710970a730f
Preston Adkins <Preston.W.Adkins-1@ou.edu>
2021-02-02 18:33:48 -0600

    Completed 2 Interpolations and response

--- Testtest/Program.cs
+++ Testtest/Program.cs
@@ -6,7 +6,31 @@ namespace Testtest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string firstName, lastName;
+            double gpa;
+
+            Console.WriteLine("Enter your first name or else >>");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter your last name or else >>");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter your gpa");
+            string gpaAsString = Console.ReadLine();
+            gpa = Convert.ToDouble(gpaAsString);
+
+            gpa = gpa + .5; //idk why but we want to bump everyone's gpa by .5
+
+            string output = "Hello" + firstName + " " + lastName + "! " +
+                " we boosted your gpa to " + gpa.ToString("N3");
+            //output = $"Hello {firstName} {lastName}! we boosted your gpa to {gpa.ToString("N3")}
+
+            Console.WriteLine(output);
+
+
+         
+
+
         }
     }
 }
