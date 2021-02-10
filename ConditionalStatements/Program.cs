b0133e98d41fa44f502f65eeb40c14dcda474189
Preston Adkins <Preston.W.Adkins-1@ou.edu>
2021-02-09 18:38:46 -0600

    Conditional Statements Used!

---
+++ ConditionalStatements/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your lucky # (whole number) >>");
+            string answer = Console.ReadLine();
+            int luckyNbr = Convert.ToInt32(answer);
+            //int luckyNbr = Convert.ToInt32(Console.Readline());
+
+            bool isAnswerAnInteger = int.TryParse(answer, out luckyNbr);
+
+            if (isAnswerAnInteger == false)
+            {
+                Console.WriteLine("Sorry that was invalid. Goodbye.");
+                Environment.Exit(-1);
+            }
+
+            int remainder = luckyNbr % 7;
+
+            if (remainder == 0)
+            {
+                Console.WriteLine($"{luckyNbr.ToString("N0")}is really a lucky number");
+            }
+            else if (luckyNbr % 13 == 0)
+            {
+                Console.WriteLine($"{luckyNbr.ToString("N0")}is really a unlucky number");
+            }
+            else
+            {
+                Console.WriteLine($"meh... {luckyNbr.ToString("N0")}");
+            }
+
+            Console.WriteLine("What is your first name >?");
+            string name = Console.ReadLine();
+
+            switch (name)
+            {
+                case "adam":
+                    Console.WriteLine("YEahh buddy!");
+                    break;
+                case "darby":
+                    Console.WriteLine("meh it could be better... now Adam is a cool name");
+                    break;
+                default:
+                    Console.WriteLine("Yeet");
+                    break;
+            }
+
+        }
+    }
+}
