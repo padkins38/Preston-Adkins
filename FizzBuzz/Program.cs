28ed1b03715e243e540c086cb0ead32d8a727652
Preston Adkins <Preston.W.Adkins-1@ou.edu>
2021-02-11 17:39:40 -0600

    Fizz Buzz Challenge

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randomNbr = rand.Next(1, 101); //Generates a random number between 1 and 100
+            int divisibleby3Remainder = randomNbr % 3;
+            bool isDivisbleBy3 = divisibleby3Remainder % 3 == 0;
+            randomNbr = 20;
+
+            if (randomNbr % 3 == 0
+               && randomNbr % 5 == 0)// == (true)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (randomNbr % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else if (randomNbr % 3 == 0)
+            {
+                Console.WriteLine("Fizz Buzz");
+            }
+            else
+            {
+                Console.WriteLine(randomNbr);
+            }
+        }
+    }
+}
---
+++ FizzBuzz/ProgramBase.cs
@@ -0,0 +1,32 @@
+﻿namespace FizzBuzz
+{
+    internal class ProgramBase
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randomNbr = rand.Next(1, 101); //Generates a random number between 1 and 100
+            int divisibleby3Remainder = randomNbr % 3;
+            bool isDivisbleBy3 = divisibleby3Remainder % 3 == 0;
+            randomNbr = 20;
+
+            if (isDivisbleBy3)// == (true)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (randomNbr % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else if (randomNbr % 3 == 0
+               && randomNbr % 5 == 0)
+            {
+                Console.WriteLine("Fizz Buzz");
+            }
+            else
+            {
+                Console.WriteLine(randomNbr);
+            }
+        }
+    }
+}
