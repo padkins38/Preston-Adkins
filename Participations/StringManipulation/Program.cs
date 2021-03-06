0c909a2bef0ed9c21821a7cdd17ac072d8674f67
Preston Adkins <Preston.W.Adkins-1@ou.edu>
2021-03-05 23:23:21 -0600

    StringManipulation FINSIHED!!

---
+++ Participations/StringManipulation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                 //01234567
+            string artist = "tAylOr SwIFt";
+            artist = artist.ToLower();  // OR string newValue = artist.ToLower();
+            //then replace "artist" in the console.writline to "newValue"
+            
+
+            string artistWithProperName = artist.ToUpper()[0] + artist.Substring(1,6) +artist.ToUpper()[7] + artist.Substring(8);// OR artist[0].ToString().ToUpper();
+            //Why do we need the substring again? 
+
+
+            //for(int i = 0; i < artistWithProperName.Length; i++)
+            //{
+            //    Console.WriteLine(artistWithProperName[i]);
+           // }
+            
+            
+            
+            
+            
+            
+            
+            Console.WriteLine(artistWithProperName);
+
+
+
+        }
+    }
+}
---
+++ Participations/String_Manipulation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace String_Manipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning";
+            Console.WriteLine($"{text}");
+
+            Console.WriteLine("What word do you want to search for in the sentence above?");
+            string searchword = Console.ReadLine();
+
+            Console.WriteLine("What word would you like to change it to?");
+            string changeword = Console.ReadLine();
+
+            if (!text.Contains(searchword))
+            {
+                Console.WriteLine($"I am sorry, {searchword} was not found in the sentence");
+
+                char[] chararray = searchword.ToCharArray();
+                for (int i = chararray.Length - 1; i >=0; i--)
+                {
+                    Console.WriteLine($"{chararray[i]}");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                string textback = text.Replace(searchword, changeword);
+                Console.WriteLine($"{textback}");
+            }
+        }
+    }
+}
