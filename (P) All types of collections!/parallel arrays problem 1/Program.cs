fc5980b3ebbf0058b4c1fc90947255e6d0d767cd
Preston Adkins <Preston.W.Adkins-1@ou.edu>
2021-03-28 22:41:50 -0500

    Working on P1 and 2

--- (P) All types of collections!/parallel arrays problem 1/Program.cs
+++ (P) All types of collections!/parallel arrays problem 1/Program.cs
@@ -7,19 +7,16 @@ namespace parallel_arrays_problem_1
         static void Main(string[] args)
         {
 
-            string answer;
+            string courseanswer;
+            string IDanswer;
+            string[] courses = new string[courseanswer];
+            
+                Console.WriteLine("input a course subject that you are taking. (ex. MATH or MIS)");
+                courseanswer = Console.ReadLine().ToUpper();
 
-            do
-            {
-                Console.WriteLine("input a course subject that you are taking. (ex. MATH or MIS)";
-                answer = Console.ReadLine().ToUpper();
+                Console.WriteLine("input the course ID used next to the course subject. (ex. 3313 or 3033)");
+                IDanswer = Console.ReadLine();
 
-             
-
-
-
-
-            } while ();
         }
     }
 }
