29532ce68b72317d6f090580b76662897058b718
Preston Adkins <Preston.W.Adkins-1@ou.edu>
2021-04-09 23:20:51 -0500

    Finished the SPEAK participation

--- Participations/Calculator - Functions/Program.cs
+++ Participations/Calculator - Functions/Program.cs
@@ -9,15 +9,29 @@ namespace Calculator___Functions
             double v1 = 0;
             double val2 = 0;
             string Answer;
+            double final = 0;
             DeveloperInformation("", "", "");
+            
+            Console.WriteLine("Would you like to Perform a new calculation? (Y/N)");
+            string newCalc = Console.ReadLine().ToLower();
+            
+            Console.WriteLine("What is your first number?");
+            v1 = Convert.ToDouble(Console.ReadLine());
             do
             {
+                Console.WriteLine("Would you like to use the results of this problem as your first variable of the next equation? (Y/N)");
+                string useLastResults = Console.ReadLine().ToLower();
+
+                if (useLastResults[0] == 'y')
+                {
+                    v1 = final;
+                }
+                else
+                final = 0;
 
                 Console.WriteLine("What Calculation do you want to want to perform? (e.g. add, subtract, divide, multiply)");
                 string calculationAsk = Console.ReadLine().ToLower();
 
-                Console.WriteLine("What is your first number?");
-                v1 = Convert.ToDouble(Console.ReadLine());
 
                 Console.WriteLine("What is your second number?");
                 val2 = Convert.ToDouble(Console.ReadLine());
@@ -25,24 +39,28 @@ namespace Calculator___Functions
                 if (calculationAsk[0] == 'a')
                 {
                     Console.WriteLine($"{v1} + {val2} == {Convert.ToString(Add(v1, val2))}.");
+                    final = v1 + val2; //I would just have used this in the cw, but I don't want to have to rewrite lol
                 }
                 else if (calculationAsk[0] == 's')
                 {
                     Console.WriteLine($"{v1} - {val2} == {Convert.ToString(Subtract(v1, val2))}.");
+                    final = v1 + val2;
                 }
                 else if (calculationAsk[0] == 'm')
                 {
                     Console.WriteLine($"{v1} * {val2} == {Convert.ToString(Multiply(v1, val2))}.");
+                    final = v1 + val2;
                 }
                 else if (calculationAsk[0] == 'd')
                 {
                     Console.WriteLine($"{v1} / {val2} == {Convert.ToString(Divide(v1, val2))}.");
+                    final = v1 + val2;
                 }
-                Console.WriteLine("Again? y/n>> ");
+                
+                Console.WriteLine("Would you like to Perform a new calculation? (Y/N)");
                 Answer = Console.ReadLine().ToLower();
             }
             while (Answer[0] == 'y');
-
             static double Add(double v1, double val2)
             //static (method returned) {name(taking in (type) {var}, " same thing " )}
             {
---
+++ Participations/Simple Method (Speak)/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Simple_Method__Speak_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            do
+            {
+                Console.WriteLine("Enter an animal (e.g. cow, giraffe, turkey)");
+                string animal = Console.ReadLine().ToLower();
+
+                string A_Sounds = AnimalSound(animal);
+                Console.WriteLine($" A {animal} makes the {A_Sounds} sound");
+
+                Console.WriteLine("would you like to try for another animal sounds?  (Y/N)  >>");
+                answer = Console.ReadLine().ToLower();
+            } 
+            while (answer[0] == 'y');
+        }
+        static string AnimalSound(string animal)
+        {
+            string sound;
+
+            if (animal == "cow")
+            {
+                sound = "MoOOooO00oO0ooo00OOo!";
+                return sound;
+            }
+            else if (animal == "giraffe")
+            {
+                sound = "HhFfff!";
+                return sound;
+            }
+            else
+            {
+                sound = "GObble gObble Gobble";
+                return sound;
+            }
+            return "sound";
+        }
+    }
+}
