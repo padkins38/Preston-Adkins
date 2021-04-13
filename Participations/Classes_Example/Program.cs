1704da1c9809ac7901308d7ea40aeef116f25e12
Preston Adkins <Preston.W.Adkins-1@ou.edu>
2021-04-13 18:22:59 -0500

    Trying to make a class. Needs fixing

---
+++ Participations/Classes_Example/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+
+namespace Classes_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //first instance or the Rectangle
+            Rectangle r1 = new Rectangle();
+            r1.Length = 8
+            r1.Width = 8;
+
+            double area = r1.CalculateArea();
+
+            Console.WriteLine($"Rectangle 1 - L:{r1.Length}, W:{r1.Width}, A:{area}, P:{r1.CalculatePerimeter()}")
+            
+            //second instance of the Rectangle
+            Rectangle r2 = new Rectangle(15, 11);
+            Console.WriteLine($"Rectangle 2 - L:{r2.Length}, W:{r2.Width}, A:{r2.CalculateArea()}, P:{r2.CalculatePerimeter()}")
+            r2.Width = 10;
+            Console.WriteLine("Changed the width to 10");
+            Console.WriteLine($"Rectangle 2 - L:{r2.Length}, W:{r2.Width}, A:{r2.CalculateArea()}, P:{r2.CalculatePerimeter()}")
+
+        }
+    }
+}
---
+++ Participations/Classes_Example/Rectangle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Example1
+{ 
+    public class Rectangle //Make sure to make it a public class as habit for next semester
+    {
+
+        //this is what the public double Lenght { get; set; } is doing in the background
+
+
+        //private double _Width;
+        //
+        //public double GetWidth()
+        //{
+        //    return _Width;
+        //}
+        //public void SetWidth(double width)
+        //{
+        //    _Width = width;
+        //}
+
+
+        public double Length { get; set; }
+        public double Width { get; set; }
+        /// <summary>
+        /// Overloaded constructor, where we know what values we want to start the width and length to
+        /// </summary>
+        /// <param name="Width"></param>
+        /// <param name="Length">Set Width and Length as values</param>
+        public Rectangle(double width, double length)
+        {
+            this.Width = width;
+            Length = length;
+        }
+
+        /// <summary>
+        /// Default/Empty constructor that sets this instance of a Rectanle to starting values
+        /// </summary>
+        public Rectangle()
+        {
+            Width = 5;   //creating every rectangle with a starting value of 5
+            Length = 5;
+        }
+
+        //CalculateArea( w:double, l:double ) :
+
+        /// <summary>
+        /// Calculates the are of this Rectangle
+        /// </summary>
+        /// <returns>The area of this Rectangle</returns>
+        public double CalculateArea()
+        {
+
+            return Length * Width;
+        }
+        /// <summary>
+        /// Calculated the Perimeter of this Rectangle
+        /// </summary>
+        /// <returns>The perimeter of this Rectangle</returns>
+        public double CalculatePerimeter()
+        {
+            return 2 * (Length + Width);
+        }
+    }
+}
