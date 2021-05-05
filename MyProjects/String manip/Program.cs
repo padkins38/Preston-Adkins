a038b176ed583131a3b1224978f87fb630a3810d
Preston Adkins <Preston.W.Adkins-1@ou.edu>
2021-05-04 19:42:43 -0500

    !!!!

---
+++ MyProjects/String manip/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace String_manip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = "Hello World!";
+            if (text.Substring(text.Length - 1) == "!")
+            {
+
+                string newtext = text.Substring(0, text.Length - 1);
+                Console.WriteLine(newtext);
+
+            }
+            else
+            {
+                Console.WriteLine("Continue to write sentences");
+            }
+
+        }
+    }
+}
