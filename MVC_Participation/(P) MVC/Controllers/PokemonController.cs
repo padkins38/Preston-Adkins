8a329c9defdfde3e43f9a7184ac782d17e6d0c37
Preston Adkins <Preston.W.Adkins-1@ou.edu>
2021-11-22 18:49:20 -0600

    finished all

---
+++ MVC_Participation/(P) MVC/Controllers/PokemonController.cs
@@ -0,0 +1,42 @@
+﻿using _P__MVC.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _P__MVC.Controllers
+{
+    public class PokemonController : Controller
+    {
+        // GET: Pokemon
+        public ActionResult Index()
+        {
+            PokemonAPI allpokemon;
+
+            using (var Client = new HttpClient())
+            {
+                var json = Client.GetStringAsync("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1100").Result;
+
+                allpokemon = JsonConvert.DeserializeObject<PokemonAPI>(json);
+            }
+
+            return View(allpokemon.results);
+        }
+        public ActionResult Info(string id)
+        {
+
+            PokemonInfo info;
+
+            using (var Client = new HttpClient())
+            {
+                var json = Client.GetStringAsync("https://pokeapi.co/api/v2/pokemon/{id}").Result;
+
+                info = JsonConvert.DeserializeObject<PokemonInfo>(json);
+            }
+
+            return View(info);
+        }
+}
---
+++ MVC_Participation/(P) MVC/Models/PokemonAPI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _P__MVC.Models
+{
+    public class Result
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+    public class PokemonAPI
+    {
+        public string count { get; set; }
+        public string next { get; set; }
+        public string previous { get; set; }
+        public List<Result> results { get; set; }
+
+    }
+    public class PokemonInfo
+    {
+        public int height { get; set; }
+        public string name { get; set; }
+        public double weight { get; set; }
+        public Sprite sprites { get; set; }
+
+    }
+    public class Sprite
+    {
+        public string back_default { get; set; }
+        public string front_default { get; set; }
+    }
+}
